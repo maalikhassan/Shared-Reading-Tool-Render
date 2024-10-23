2a9ca842748178f2d4272f17cb56f09923f2a721
Glitch (glitch-hello-node) <none>
2024-10-23 19:32:49 +0000

    🌼🎱 Checkpoint ./server.js:71326094/1261 ./package.json:71326094/698

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ wss.on('connection', ws => {
   });
 });
 
-server.listen(process.env.PORT || 8080, () => {
-  console.log('Server is listening on port 8080');
+const port = process.env.PORT || 8080;
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
+
